ceda4123fef0b4fc6ccdbf1add7791a055e990af
Tarık Hamarat <trkhamarat@gmail.com>
2025-02-01 16:52:39 +0300

    Create 01_Initial.py

---
+++ 01-Algoritma/01_Initial.py
@@ -0,0 +1,42 @@
+#Bu benim ilk ekrana çıktı verdiğim kod
+
+print("Merhaba Dünya!  Bu bizim ilk kodumuz.")
+
+
+"""
+Birden fazla 
+   satır içeren yorumlar 
+        burada yer alacak.
+"""
+
+
+#İlk dersimizde algoritmalara başladık.Neler olduğunu öğrendik.Pratik yaptık.
+#İlk dersimizde algoritmalara başladık.Neler olduğunu öğrendik.Pratik yaptık.
+# İlk dersimizde algoritmalara başladık.Neler olduğunu öğrendik.Pratik yaptık.
+# İlk dersimizde algoritmalara başladık.Neler olduğunu öğrendik.Pratik yaptık.
+# İlk dersimizde algoritmalara başladık.Neler olduğunu öğrendik.Pratik yaptık.
+# İlk dersimizde algoritmalara başladık.Neler olduğunu öğrendik.Pratik yaptık.
+# İlk dersimizde algoritmalara başladık.Neler olduğunu öğrendik.Pratik yaptık.
+# İlk dersimizde algoritmalara başladık.Neler olduğunu öğrendik.Pratik yaptık.
+
+
+
+
+"""
+
+Algoritma
+Herhangi bir işlemin son buluncaya kadar takip ettiği mantıklı adımlar bütünü
+Mutlaka bir son olmalı.
+Şema veya Yazı olarak algoritmayı oluşturabilirsiniz.
+
+"""
+
+
+
+
+
+
+
+
+
+
