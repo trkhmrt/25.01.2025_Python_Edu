7fc2e09b3d2316397a862f995fbba4bc3afa1c1c
Tarık Hamarat <trkhamarat@gmail.com>
2025-03-08 16:54:12 +0300

    TryExcept_Functions

---
+++ 09_Try_Except/00_Try_Except.py
@@ -0,0 +1,172 @@
+"""
+Try-Except
+Hata yakalama için kullanılan kod bloğudur.
+Farklı hata tiplerinde uygulamanın çökmesi söz konusu olabilir.Bunun önüne geçmek ve hataların detaylarını daha net görebilmek için bu kod bloğundan faydalanırız.
+
+try:
+    Hata alma ve uygulamayı çökertme potansiyeline sahip kodlar try içerisinde çalıştırılır.
+except:
+    Hata meydana geldiğinde çalıştırılacak kodlar burada yer alır.
+
+HataTürleri
+
+ZeroDivisionError => Bir sayıyı sıfıra bölerken bu hata alınır.
+
+IndexError => Listelerde geçersiz indeks numarası kullanıldığında bu hata alınır.
+
+ValueError => Talep edilen veri tipiyle yada dönüştürülmek istenen veri tipiyle okunan verinin uyumsuz olduğu durumlarda bu hata döndürülür.
+
+TypeError => Birbiriyle uyumsuz veri tiplerinin işleme sokulması sonucu alınır.
+
+ModuleNotFoundError => dahil edilmek istenen modül bulunmuyorsa bu hata alınır.
+
+FileNotFounderror => Dosya bulunamadığında oluşur.
+
+SyntaxError => Python aid kodların söz diziminde hata yapıldığında alınır
+
+IndentationError => if , def , while , try gibi indentation kuralına uyulması gereken yerlrde uyulmadığında bu hata alınır.
+"""
+
+####### ZeroDivisionError #######
+# sayi1 = 10
+# sayi2 = 0
+#
+# print(sayi1/sayi2)
+# print("Merhaba")
+
+# try:
+#     sayi1 = 10
+#     sayi2 = 0
+#     print(sayi1 / sayi2)
+#     print("Merhaba")
+# except:
+#     print("Sıfıra bölünme hatası")
+#
+
+######## IndexError #########
+
+# list = [10,20]
+# print(list[10])
+# try:
+#     print(list[10])
+# except:
+#     print("Hata meydana geldi")
+#
+# print("Başarılı şekilde sonlandırıldı.")
+
+
+######## ValueError #########
+# try:
+#     sayi = int(input("Bir sayı giriniz"))
+# except:
+#     print("Hata meydana geldi")
+
+
+
+####### TYPE ERROR ########
+# sayi = 10
+# isim = "Tarık"
+#
+# sonuc = sayi + isim
+
+
+
+######## ModuleNotFoundError  ######
+#import trk
+#
+# try:
+#     import trk
+# except:
+#     print("Hata meydana geldi")
+
+
+##### FileNotFoundError ######
+#dosya = open("olmayan_dosya.txt","r")
+# try:
+#     dosya = open("olmayan_dosya.txt","r")
+# except:
+#     print("Bir hata meydana geldi")
+
+
+
+
+###### Exception as e ile hata mesajı içeriğini görüntüleme #######
+# try:
+#     # sayi1 = 10
+#     # sayi2 = 0
+#     # print(sayi1 / sayi2)
+#     list = []
+#     print(list[0])
+# except Exception as e:
+#     print("Hata meydana geldi.",e)
+
+
+
+###### HATA TÜRLERİNİ AYRI EXCEPTIONLARDA ELE ALMA ##########
+# try:
+#     # sayi1 = 10
+#     # sayi2 = 0
+#     # print(sayi1 / sayi2)
+#     list = []
+#     print(list[0])
+# except ZeroDivisionError:
+#     print("Bölen Sayı sıfır olamaz...")
+# except IndexError:
+#     print("Lütfen dizi sınırları içerisinde index numarası veriniz.")
+
+
+####### FINALLY BLOĞU (Uygulamada hata alınsada alınmasada finally çalışır.) #######
+# try:
+#     list = []
+#     #print(list[0])
+#     print("Herhangi bir hata meydana gelmedi")
+# except:
+#     print("Hata meydana geldi")
+# finally:
+#     print("Hatalı hatasız her türlü ben çalışırım.")
+
+
+######## SyntaxError   #####
+#print("
+
+
+
+######## IndentationError  ########
+# if True:
+# print("Merhaba")
+
+
+####### KEYERROR => Sözlükte olmayan bir anahtar çağrıldığında hata verir ######
+#
+# sozluk = {"ad":"Tarık"}
+# print(sozluk["yas"])
+
+
+###### ATTRIBUTEERROR => olmayan bir metodu veya özelliği çağırmaya çalıştığımızda ######
+# yas = 10
+# yas.count()
+
+######## NAME_ERROR => Tanımlanmamış Değişken kullanıldığında  #######
+#print(x)  #x herhangi bir yerde tanımlı değil
+
+
+
+
+######## RAISE İLE BİRLİKTE KENDİ HATALARIMIZI FIRLATMA #######
+
+
+
+
+# try:
+#     yas = 17
+#
+#     if yas < 18:
+#         raise ValueError("Yaş 18 den küçük")
+# except ValueError as e:
+#     print(e)
+
+
+
+
+
+
---
+++ 10_Functions/00_Functions.py
@@ -0,0 +1,168 @@
+"""
+Fonksiyonlar
+
+herhangi bir kod bloğu bir yerde birden fazla kez yazılmışsa burada bir sorun var demektir.Biz yazdığımız kodları olabildiğince sade tutmaya çalışırız.Aynı kod satırlarını tekrar etmek istemeyiz.
+Çünkü bu kodun okunabilirliğini düşürür.
+
+fonksiyon nasıl tanımlanır.
+
+def fonksiyon_adi():
+    fonksiyon çağrılırsa buradaki kodlar çalıştırılacak.
+
+
+Metotlar Çağrıldıklarında tanımlandıkları yere giderler.Orada çalıştıktan sonra ise çağrıldıkları yere geri dönerler.
+
+Metotlar çağrılmadan çalışmazlar.
+
+
+METOT TÜRLERİ
+
+1-Geri değer döndüren  ve Geri değer döndürmeyen
+
+Geri değer döndürmeyen => metot çalışır bütün işleri kendi içinde halleder dışarı bir vefri paylaşmaz.
+    -  Parametreli
+    - Parametresiz
+Geri değer döndüren => bu metot türleri istenilen veri tipindeki veriyi çağrıldığı yere taşır.Bunu return keywordu ile yapar.
+    -  Parametreli
+    -  Parametresiz
+
+
+"""
+####### PARAMETRESİZ METOT #########
+# def ekranaMesajYaz():
+#     print(f"Merhaba Tarık")
+#     print("Sisteme Hoşgeldiniz")
+#     print("Bakiyeniz")
+#
+#
+# ekranaMesajYaz()
+#
+# ekranaMesajYaz()
+
+
+# for i in range(3):
+#     ekranaMesajYaz()
+
+
+####### PARAMETRELİ METOT #######
+
+# def ekranaMesajYaz(isim):
+#     print(f"Merhaba {isim}")
+#     print("Sisteme Hoşgeldiniz")
+#     print("Bakiyeniz")
+#
+#
+#
+#
+# ekranaMesajYaz("Tarık")
+# ekranaMesajYaz("Yusuf")
+# ekranaMesajYaz("Ozan")
+# ekranaMesajYaz("Sercan")
+#
+
+
+
+##### GERİ DEĞER DÖNDÜRMEYEN ######
+# return kullanmadan metodun kendisini yazdırmak istediğimizde none denen bir hata aldık.
+# def sayilariTopla(s1,s2):
+#     print(s1+s2)
+#
+#
+# print(sayilariTopla(10,20))
+
+
+##### GERİ DEĞER DÖNDÜREN METOT #######
+
+# def sayilariTopla(s1,s2):
+#     return s1+s2
+#
+# veri = sayilariTopla(10,20)
+#
+#
+# print(veri)
+
+
+
+#### RETURN İFADESİ METOTLARDA BİR ANLAMDA BREAK GİBİ İŞLEV GÖRÜR #######
+
+##Return ifadesiyle karşılaşan metot returnun önündeki değeri çağrıldığı yere taşır bir nevi break gibi çalışır.
+# def sayilariTopla(s1,s2):
+#     return s1+s2
+#     print("Merhaba Dünya")
+#
+# veri = sayilariTopla(10,20)
+#
+#
+# print(veri)
+
+
+
+
+
+####### TRY EXCEPT YAPISI İLE METOTLARIN KULLANIMI
+
+
+# def bolmeIslemi():
+#     print(10/0)
+#
+#
+# try:
+#     bolmeIslemi()
+#     print("Merhaba")
+# except:
+#     print("Hata meydana geldi.")
+
+
+# def indexHataMetot():
+#     list=[]
+#     print(list[10])
+#
+#
+# def bolmeIslemi():
+#     print(10/2)
+#     indexHataMetot()
+#
+#
+#
+#
+# try:
+#     bolmeIslemi()
+#     print("Merhaba")
+# except ZeroDivisionError:
+#     print("Sıfıra bölünme hatası.")
+# except IndexError:
+#     print("Liste index aralığı dışında değer.")
+
+
+
+
+#### ÖRNEK #####
+"""
+Kullanıcıdan 3 adet veri alınız;
+1-İsim
+2-Yaş
+3-Hobiler
+
+Bunları metot metot içerisinde  İsimli kişinin yaşı şudur ve hobileride bunlardır şeklinde string  bir mesaj oluşturun ardından bunu geri döndürün
+"""
+
+# def bilgileriYaz(name,age,hobbies):
+#     return f"{name} kişisinin yaşı {age}.Hobileri {hobbies}"
+#
+#
+#
+# isim = input("İsim giriniz")
+#
+# yas = input("Yaş giriniz")
+#
+# hobiler = [input("1.Hobi giriniz"),input("2.Hobi giriniz"),input("3.Hobi giriniz")]
+#
+#
+# print(bilgileriYaz(isim,yas,hobiler))
+
+
+
+
+
+
+
